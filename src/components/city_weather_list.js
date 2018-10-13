1c094ec179c6c45007b941a0f1c66f36d4e6ac2c
jagmeet <jagmeet.alagh@gmail.com>
2018-10-13 13:48:34 +0530

    added charts for weather

--- src/components/city_weather_list.js
+++ src/components/city_weather_list.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { Sparklines, SparklinesLine } from 'react-sparklines';
+
 class CityWeatherList extends Component {
 
     renderWeather() {
         return this.props.weather.map( (data) => {
+            let cityTemp = data.list.map(cityData => cityData.main.temp);
+            let cityPressure = data.list.map(cityData => cityData.main.pressure);
+            let cityHumidity = data.list.map(cityData => cityData.main.humidity);
+
             return (
                 <tr key={ data.city.name }>
                     <td>{ data.city.name }</td>
+                    <td >
+                        <Sparklines data={cityTemp} height={100}>
+                            <SparklinesLine color='blue' />
+                        </Sparklines>
+                    </td>
+                    <td >
+                    <Sparklines data={cityPressure} height={110}>
+                            <SparklinesLine color='red' />
+                        </Sparklines>
+                    </td>
+                    <td>
+                    <Sparklines data={cityHumidity} height={110}>
+                            <SparklinesLine color='green' />
+                        </Sparklines>
+                    </td>
                 </tr>
             );
         });
